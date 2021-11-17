13462009cf215eb4f241888966a1c8572ce2a2c8
Isaiah-Lam <isaiah@lamison.net>
2021-11-17 14:42:34 -0500

    menu styling and partial content

---
+++ pizzahub.js
@@ -0,0 +1,10 @@
+function openCategory(id, items) {
+    if($(items).css('display') === 'block') {
+        $(id).css("overflow", "hidden");
+        $(items).slideUp(500);
+    }
+    else {
+        $(id).css("overflow", "initial");
+        $(items).slideDown(500);
+    }
+}
